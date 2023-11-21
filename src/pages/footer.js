3f7f736c0d8721c89ca48361afcde1c768039f2a
faizulhassanRien <faizulhassan.rien@gmail.com>
2023-11-21 19:10:11 +0600

    add two page

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -21,7 +21,7 @@ const Footer = () => {
                                 width={120}
                                 alt="" />
                         </figure>
-                        <p className="w-40 text-white text-[10px] p-1 border-b-2  border-white  mb-4">
+                        <p className="w-40 text-white text-[11px] p-1 border-b-2  border-white  mb-4">
                             <b>The KOW Company</b> is one of the
                             leading image and video editing
                             company for businesses all over
@@ -54,47 +54,47 @@ const Footer = () => {
                             </a>
                         </div>
 
-                        <button className="bg-white px-8 py-2 flex justify-center items-center gap-2 rounded-3xl text-xs">Language <i class="fa-solid pb-1 fa-sort-down"></i></button>
+                        <button className="bg-white  px-6 py-2 mt-2 gap-2 rounded-3xl text-xs">Language <i class="fa-solid pb-1 fa-sort-down"></i></button>
                     </div>
 
                     <div className="flex flex-col text-white lg:items-start md:-ml-16 md:items-start text-start gap-1">
-                        <span className="text-lg font-semibold text-[white]  ">HOME</span>
+                        <span className="text-lg font-semibold text-[white] mb-2 ">HOME</span>
                         <a className="link link-hover text-xs">2D</a>
-                        <a className="link link-hover text-sm">3D</a>
-                        <a className="link link-hover text-sm">AI</a>
-                        <a className="link link-hover text-sm">Integrations</a>
-                        <a className="link link-hover text-sm">Blueprint</a>
-                        <a className="link link-hover text-sm">Industry Trends</a>
-                        <a className="link link-hover text-sm">Blog</a>
+                        <a className="link link-hover text-xs">3D</a>
+                        <a className="link link-hover text-xs">AI</a>
+                        <a className="link link-hover text-xs">Integrations</a>
+                        <a className="link link-hover text-xs">Blueprint</a>
+                        <a className="link link-hover text-xs">Industry Trends</a>
+                        <a className="link link-hover text-xs">Blog</a>
 
                     </div>
                     <div className="flex lg:items-start text-white md:items-start flex-col md:-ml-16 text-start  gap-1  lg:-ml-0">
-                        <span className="text-lg font-semibold ">Learn more</span>
-                        <a className="link link-hover text-sm">Magic Brush</a>
-                        <a className="link link-hover text-sm">Individuals</a>
-                        <a className="link link-hover text-sm">Photographers</a>
-                        <a className="link link-hover text-sm">Marketing</a>
-                        <a className="link link-hover text-sm">Developers</a>
-                        <a className="link link-hover text-sm">Ecommerce</a>
+                        <span className="text-lg font-semibold mb-2">Learn more</span>
+                        <a className="link link-hover text-xs">Magic Brush</a>
+                        <a className="link link-hover text-xs">Individuals</a>
+                        <a className="link link-hover text-xs">Photographers</a>
+                        <a className="link link-hover text-xs">Marketing</a>
+                        <a className="link link-hover text-xs">Developers</a>
+                        <a className="link link-hover text-xs">Ecommerce</a>
                     </div>
                     <div className="flex text-white lg:items-start md:items-start md:-ml-16 flex-col text-left gap-1  lg:-ml-0">
-                        <span className="text-lg font-semibold ">Support</span>
-                        <a className="link link-hover text-sm">Help & FAQs</a>
-                        <a className="link link-hover text-sm">Refunds</a>
-                        <a className="link link-hover text-sm">Platform Status</a>
-                        <a className="link link-hover text-sm">Privacy Policy</a>
-                        <a className="link link-hover text-sm">Terms & Conditions</a>
-                        <a className="link link-hover text-sm">Contact us</a>
-                        <a className="link link-hover text-sm">Sign up</a>
+                        <span className="text-lg font-semibold mb-2">Support</span>
+                        <a className="link link-hover text-xs">Help & FAQs</a>
+                        <a className="link link-hover text-xs">Refunds</a>
+                        <a className="link link-hover text-xs">Platform Status</a>
+                        <a className="link link-hover text-xs">Privacy Policy</a>
+                        <a className="link link-hover text-xs">Terms & Conditions</a>
+                        <a className="link link-hover text-xs">Contact us</a>
+                        <a className="link link-hover text-xs">Sign up</a>
                     </div>
                     <div className="text-center ml-20 md:ml-0 lg:ml-0">
-                        <span className="font-bold text-white text-lg pb-2 ml-8 lg:ml-5">
+                        <span className="font-bold text-white text-lg mb-4 ml-8 lg:-ml-16">
                             Our Address
                         </span>
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10329.283883735325!2d90.39304891556642!3d23.749043274767246!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc1fb12dbea63beca!2sCutOutWiz!5e0!3m2!1sbn!2sbd!4v1639129804206!5m2!1sbn!2sbd"
-                            width="75%"
-                            height="75%"
+                            width="70%"
+                            height="70%"
                             style={{ border: "0" }}
                             allowfullscreen=""
                             loading="lazy"

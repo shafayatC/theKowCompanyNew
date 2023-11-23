442000196f2aa0f62259e4749201eafe96db62f6
shafayat18 <shafayatplay@gmail.com>
2023-11-23 18:31:34 +0600

    upd

--- src/component/slider/divSlider.js
+++ src/component/slider/divSlider.js
@@ -20,15 +20,20 @@ const DivSlider = ({width, currentPos, childLength, children}) => {
 
     useEffect(() => {
        if (sliderRef.current) {
-        console.log(sliderRef); 
-          setSliderWidth((slwidth)=>sliderRef.current.offsetWidth);
+            console.log(sliderRef.current.children[0].children); 
+            setSliderWidth((slwidth)=>sliderRef.current.offsetWidth);
+           
+          for (let index = 0; index < childLength; index++) {
+            sliderRef.current.children[0].children[index].style.width = sliderRef.current.offsetWidth+'px';
+            
+          }
        }
     }, [sliderId]);
     return (
         <>
         {console.log(getSliderWith)}
         <div ref={sliderRef} id={sliderId} className='relative overflow-hidden' style={{height: '100%'}} >
-            <div className='transition-all duration-300 absolute top-0 left-0 h-full' style={{width: (getSliderWith*childLength)+'px', left: currentPos+'px'}}>
+            <div className='transition-all duration-300 absolute top-0 left-0 h-full' style={{width: (getSliderWith*childLength)+'px', left: (getSliderWith * currentPos)+'px'}}>
             {children}
             </div>
         </div>
